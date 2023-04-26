8c473079e9feec7e5b1b3ebb86c515695743d172
Mech24689 <gabrieltrem102030@gmail.com>
2023-04-26 20:44:23 -0300

    Fiz a estilização da tela login.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,24 @@
 btnAcessar = document.querySelector('.acessar')
+inputLogin = document.querySelector('#login')
+inputSenha = document.querySelector('#password')
+titleLogin = document.querySelector('.titleLogin')
+titleSenha = document.querySelector('.titleSenha')
 
 btnAcessar.addEventListener('click', ()=>{
-    window.close()
+    inputLogin.addEventListener('focus', ()=>{
+        titleLogin.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
+        titleSenha.style.boxShadow = 'none'
+        console.log(document.hasFocus())
+        })
+    
+    inputSenha.addEventListener('focus', ()=>{
+        titleSenha.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
+        titleLogin.style.boxShadow = 'none'
+        })
+
+        if (inputLogin.value == 'admin' && inputSenha.value == 'admin'){
+            window.close()
+        }else{
+            alert('Login ou Senha incorreta!')
+        }
 })
