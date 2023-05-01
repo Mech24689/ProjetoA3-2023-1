2ecca9e6fa72b23e1e04a6a03f3e8d6446f807ff
Mech24689 <gabrieltrem102030@gmail.com>
2023-05-01 12:13:27 -0300

    atualização na tela login

--- js/login.js
+++ js/login.js
@@ -6,17 +6,17 @@ titleSenha = document.querySelector('.titleSenha')
 key = 'tipo da conta'
 tipo = localStorage.getItem(key)
 
-btnAcessar.addEventListener('click', ()=>{
-    inputLogin.addEventListener('focus', ()=>{
-        titleLogin.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
-        titleSenha.style.boxShadow = 'none'
-        })
-    
-    inputSenha.addEventListener('focus', ()=>{
-        titleSenha.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
-        titleLogin.style.boxShadow = 'none'
-        })
+inputLogin.addEventListener('focus', ()=>{
+    titleLogin.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
+    titleSenha.style.boxShadow = 'none'
+    })
 
+inputSenha.addEventListener('focus', ()=>{
+    titleSenha.style.boxShadow = '0 0 10px 3px black, inset 0 0 10px 3px black'
+    titleLogin.style.boxShadow = 'none'
+    })
+
+btnAcessar.addEventListener('click', ()=>{
         if (inputLogin.value == 'admin' && inputSenha.value == 'admin'){
             localStorage.setItem(key, 'premium')
             window.location.href = 'index.html'
