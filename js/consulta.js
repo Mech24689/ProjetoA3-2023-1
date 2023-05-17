95d65b099a5cc1d9eee382f6324eb83bfca3ba05
Mech24689 <gabrieltrem102030@gmail.com>
2023-05-17 19:20:55 -0300

    conclusão do sistema de filtragem.

--- js/consulta.js
+++ js/consulta.js
@@ -1,5 +1,26 @@
 btnVoltar = document.querySelector('.btn-voltar')
+cards = document.querySelectorAll('.cards')
+textFilter = document.querySelector('#filter')
 
 btnVoltar.addEventListener('click', ()=>{
     window.location.href = 'index.html'
 })
+
+textFilter.addEventListener('input', ()=>{
+    for(let card of cards){
+        especialidade = card.querySelector('.areaMedico').innerText
+        filter = textFilter.value
+        
+        if(filter == ''){
+            card.style.display = 'flex'
+        }else{
+            if(!especialidade.includes(filter)){
+                card.style.display = 'none'
+            }else{
+                card.style.display = 'flex'
+            }
+        }
+    }
+})
+
+
